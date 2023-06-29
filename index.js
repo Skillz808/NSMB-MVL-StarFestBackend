24e1b984bfec51e00d69f3ca7d1fc2a526a45368
Skillz <aaronleo5000@gmail.com>
2023-06-29 09:41:21 +0100

    Test hosting on VPS

--- index.js
+++ index.js
@@ -13,114 +13,94 @@ const starfestsFile = './StarFestInfo.json';
 
 app.use(express.json());
 
+// Middleware to check the API key
 const requireApiKey = (req, res, next) => {
   const apiKey = req.headers['x-api-key'];
-  console.log('API key:', apiKey); // Log the API key to the console
 
-  // Check if the API key matches the expected value
   if (apiKey && apiKey === apiKeys) {
-    next(); // API key is valid, continue to the next middleware/route handler
+    next();
   } else {
-    res.status(401).send('Unauthorized'); // API key is invalid, send a 401 Unauthorized response
+    res.status(401).send('Unauthorized');
   }
 };
 
-app.get('/api/starfests', requireApiKey, (req, res) => {
-  console.log("request received");
-  // Read the starfests data from the file
-  fs.readFile(starfestsFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal server error');
-      return;
-    }
+// Route to get the list of starfests
+app.get('/api/starfests', requireApiKey, async (req, res) => {
+  const starfests = await readStarfestsFromFile();
 
-    // Parse the JSON data from the file and send it as the response
-    res.json(JSON.parse(data));
-  });
+  res.json(starfests);
 });
 
-app.put('/starfests/increment-team1-count', requireApiKey, (req, res) => {
-  fs.readFile(starfestsFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal server error');
-      return;
-    }
-
-    // Parse the JSON data from the file
-    const starfests = JSON.parse(data);
-
-    // Find the current ongoing starfest
-    const currentStarfest = starfests.starfests.find(starfest => {
-      const now = new Date();
-      const startTime = new Date(starfest.startTime);
-      const endTime = new Date(starfest.endTime);
-      return now >= startTime && now <= endTime;
-    });
-
-    if (!currentStarfest) {
-      res.status(400).send('No ongoing starfest found');
-      return;
-    }
-
-    // Increment the team1Count property of the current ongoing starfest
-    currentStarfest.team1Count++;
-
-    // Save the updated data to the file
-    fs.writeFile(starfestsFile, JSON.stringify(starfests), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Internal server error');
-        return;
-      }
-
-      // Send a success response
-      res.status(200).send('Team 1 count incremented successfully');
-    });
-  });
+// Route to get the current date and time
+app.get('/api/currentDateTime', requireApiKey, (req, res) => {
+  const dateTime = new Date().toISOString();
+  res.send(dateTime);
 });
 
-app.put('/starfests/increment-team2-count', requireApiKey,(req, res) => {
-  fs.readFile(starfestsFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal server error');
-      return;
-    }
+// Route to increment the team1 count
+app.put('/starfests/increment-team1-count', requireApiKey, async (req, res) => {
+  const starfests = await readStarfestsFromFile();
 
-    // Parse the JSON data from the file
-    const starfests = JSON.parse(data);
+  const currentStarfest = findCurrentStarfest(starfests);
 
-    // Find the current ongoing starfest
-    const currentStarfest = starfests.starfests.find(starfest => {
-      const now = new Date();
-      const startTime = new Date(starfest.startTime);
-      const endTime = new Date(starfest.endTime);
-      return now >= startTime && now <= endTime;
-    });
+  if (!currentStarfest) {
+    res.status(400).send('No ongoing starfest found');
+    return;
+  }
 
-    if (!currentStarfest) {
-      res.status(400).send('No ongoing starfest found');
-      return;
-    }
+  currentStarfest.team1Count++;
 
-    // Increment the team2Count property of the current ongoing starfest
-    currentStarfest.team2Count++;
+  await writeStarfestsToFile(starfests);
 
-    // Save the updated data to the file
-    fs.writeFile(starfestsFile, JSON.stringify(starfests), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Internal server error');
-        return;
-      }
-
-      // Send a success response
-      res.status(200).send('Team 2 count incremented successfully');
-    });
-  });
+  res.status(200).send('Team 1 count incremented successfully');
 });
+
+// Route to increment the team2 count
+app.put('/starfests/increment-team2-count', requireApiKey, async (req, res) => {
+  const starfests = await readStarfestsFromFile();
+
+  const currentStarfest = findCurrentStarfest(starfests);
+
+  if (!currentStarfest) {
+    res.status(400).send('No ongoing starfest found');
+    return;
+  }
+
+  currentStarfest.team2Count++;
+
+  await writeStarfestsToFile(starfests);
+
+  res.status(200).send('Team 2 count incremented successfully');
+});
+
+// Helper function to read the starfests data from the file
+async function readStarfestsFromFile() {
+  const data = await fs.readFileSync(starfestsFile, 'utf8');
+  return JSON.parse(data);
+}
+
+// Helper function to find the current ongoing starfest
+function findCurrentStarfest(starfests) {
+  const now = new Date();
+
+  for (const starfest of starfests.starfests) {
+    const startTime = new Date(starfest.startTime);
+    const endTime = new Date(starfest.endTime);
+
+    if (now >= startTime && now <= endTime) {
+      return starfest;
+    }
+  }
+
+  return null;
+}
+
+// Helper function to write the starfests data to the file
+async function writeStarfestsToFile(starfests) {
+  const data = JSON.stringify(starfests, null, 2);
+  await fs.writeFileSync(starfestsFile, data);
+}
+
 app.listen(process.env.PORT || port, () => {
   console.log(`API listening at http://localhost:${port}`);
 });
